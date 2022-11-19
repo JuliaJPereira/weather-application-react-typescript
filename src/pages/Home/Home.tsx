898125b73d50bb6befe58ba125f586f54b3477a0
Julia Pereira <juliapereira.odc@gmail.com>
2022-11-19 13:28:44 -0300

    API OpenWeather

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,8 +1,16 @@
 import { Container, Button, Box, TextField, Typography } from "@mui/material";
 import { useTranslation } from "react-i18next";
+import { useGetWeather } from "../../services/index";
 
 function Home() {
   const { t } = useTranslation();
+
+  const { getWeather } = useGetWeather();
+
+  const onClick = async () => {
+    await getWeather();
+  };
+
   return (
     <Container>
       <Box
@@ -13,8 +21,14 @@ function Home() {
         }}
       >
         <Typography variant="h4">{t("HELLO_WORLD")}</Typography>
-        <TextField id="outlined-basic" label={t('SEARCH_BY_CITY')} variant="outlined" />
-        <Button variant="contained">{t('SEARCH_BY_CITY')}</Button>
+        <TextField
+          id="outlined-basic"
+          label={t("SEARCH_BY_CITY")}
+          variant="outlined"
+        />
+        <Button variant="contained" onClick={onClick}>
+          {t("SEARCH_BY_CITY")}
+        </Button>
       </Box>
     </Container>
   );
---
+++ src/services/index.ts
@@ -0,0 +1,17 @@
+import axios from "axios";
+
+export const useGetWeather = () => {
+  const getWeather = async () => {
+
+    let response1 = await axios.get(
+      "https://api.openweathermap.org/geo/1.0/direct?q=Blumenau&limit=1&appid=e83ae355fb450341a30c6df3a60135d7"
+    );
+
+    let response2 = await axios.get(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${response1.data[0].lat}&lon=${response1.data[0].lon}&appid=e83ae355fb450341a30c6df3a60135d7&units=metric`
+    );
+    console.log(response2);
+  };
+
+  return { getWeather };
+};
